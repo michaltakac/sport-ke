f8607875b791b77bc24e91947dbbdadaf8f9a793
Michal Takac <takacmichal92@gmail.com>
2015-11-29 02:13:30 +0100

    slovak login system

--- both/accounts/config.js
+++ both/accounts/config.js
@@ -3,46 +3,50 @@
  *
  */
 AccountsTemplates.configure({
-    defaultLayout: 'appLayout',
-	defaultLayoutRegions: {
-        header: '_header',
-        footer: '_footer'
-    },
-    defaultContentRegion: 'area',
+  defaultLayout: 'appLayout',
+  defaultContentRegion: 'area',
 
-    showForgotPasswordLink: true,
-    overrideLoginErrors: true,
-    enablePasswordChange: true,
-    sendVerificationEmail: false,
+  showForgotPasswordLink: true,
+  overrideLoginErrors: true,
+  enablePasswordChange: true,
+  sendVerificationEmail: false,
 
-    //enforceEmailVerification: true,
-    //confirmPassword: true,
-    //continuousValidation: false,
-    //displayFormLabels: true,
-    //forbidClientAccountCreation: false,
-    //formValidationFeedback: true,
-    //homeRoutePath: '/',
-    //showAddRemoveServices: false,
-    //showPlaceholders: true,
+  //enforceEmailVerification: true,
+  //confirmPassword: true,
+  //continuousValidation: false,
+  //displayFormLabels: true,
+  //forbidClientAccountCreation: false,
+  //formValidationFeedback: true,
+  //homeRoutePath: '/',
+  //showAddRemoveServices: false,
+  //showPlaceholders: true,
 
-    negativeValidation: true,
-    positiveValidation:true,
-    negativeFeedback: false,
-    positiveFeedback:false,
+  negativeValidation: true,
+  positiveValidation:true,
+  negativeFeedback: false,
+  positiveFeedback:false,
 
-    // Privacy Policy and Terms of Use
-    //privacyUrl: 'privacy',
-    //termsUrl: 'terms-of-use',
+  // Privacy Policy and Terms of Use
+  //privacyUrl: 'privacy',
+  //termsUrl: 'terms-of-use',
 });
 
 /**
  * Routes
  *
  */
-//AccountsTemplates.configureRoute('changePwd');
+AccountsTemplates.configureRoute('changePwd', {
+  path: '/zmena-hesla',
+});
 //AccountsTemplates.configureRoute('enrollAccount');
-//AccountsTemplates.configureRoute('forgotPwd');
+AccountsTemplates.configureRoute('forgotPwd', {
+  path: '/zabudnute-heslo',
+});
 AccountsTemplates.configureRoute('resetPwd');
-AccountsTemplates.configureRoute('signIn');
-AccountsTemplates.configureRoute('signUp');
-//AccountsTemplates.configureRoute('verifyEmail');
+AccountsTemplates.configureRoute('signIn', {
+  path: '/prihlasenie',
+});
+AccountsTemplates.configureRoute('signUp', {
+  path: '/registracia',
+});
+AccountsTemplates.configureRoute('verifyEmail');
---
+++ server/email/smtp.js
@@ -0,0 +1,3 @@
+Meteor.startup(function () {
+  process.env.MAIL_URL = 'smtp://postmaster%40mg.runelytics.com:f88acd0f6e0734f64b61b16432f9c6b2@smtp.mailgun.org:25';
+});
